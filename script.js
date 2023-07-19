ffb39c61207177ef2c9c7b678405cbdc0643270e
YarramsettyRamyasri <126143858+YarramsettyRamyasri@users.noreply.github.com>
2023-07-19 12:08:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function bulboff(){
+    document.getElementById("bulb").src="https://m.media-amazon.com/images/I/51bCZTw1HlL._AC_UF1000,1000_QL80_.jpg";
+}
+function bulbon(){
+    document.getElementById("bulb").src="https://img.freepik.com/free-vector/realistic-light-bulb-with-electricity_23-2149129410.jpg?w=2000";
+}
